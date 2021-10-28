42cfb6842795a982aefc359703290feb58e0fe89
“Alejandro <john.linares.mt@correo.usa.edu.co>
2021-10-28 17:39:37 -0500

    actualizar

--- src/main/java/co/edu/usa/Reto3/InterfaceReservas.java
+++ src/main/java/co/edu/usa/Reto3/InterfaceReservas.java
@@ -4,6 +4,9 @@
  */
 package co.edu.usa.Reto3;
 
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -11,5 +14,13 @@ import org.springframework.data.repository.CrudRepository;
  * @author Jhon Alejandro
  */
 public interface InterfaceReservas extends CrudRepository<Reservas,Integer>{
+    
+    public List<Reservas> findAllByStatus(String status);
+    
+    public List<Reservas> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
+    
+    // select clientId, count(*) as "total" from reservacion group by cliendId order by total desc;
+    @Query ("SELECT c.client, COUNT(c.client) from Reservas AS c group by c.client order by COUNT(c.client)DESC")
+    public List<Object[]> countTotalReservationsByClient();
     
 }
--- src/main/java/co/edu/usa/Reto3/RepositorioReservas.java
+++ src/main/java/co/edu/usa/Reto3/RepositorioReservas.java
@@ -4,6 +4,8 @@
  */
 package co.edu.usa.Reto3;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,4 +35,21 @@ public class RepositorioReservas {
     public void delete(Reservas reservation){
         crud4.delete(reservation);
     }
+     public List<Reservas> ReservacionStatus (String status){
+         return crud4.findAllByStatus(status);
+     }
+     
+     public List<Reservas> ReservacionTiempo (Date a, Date b){
+         return crud4.findAllByStartDateAfterAndStartDateBefore(a, b);
+     }
+   
+     public List<ContadorClientes> getTopClientes(){
+         List<ContadorClientes> res=new ArrayList<>();
+         List<Object[]>report = crud4.countTotalReservationsByClient();
+         for(int i=0; i<report.size();i++){
+             res.add(new ContadorClientes((Long)report.get(i)[1],(Cliente) report.get(i)[0]));
+         
+         }
+         return res;
+     }
 }
